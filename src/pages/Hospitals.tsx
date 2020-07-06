aa5997160270d3a927b26db972a1cd3fe913b498
AdityaPramar1208 <adipramar@gmail.com>
2020-07-06 19:56:33 +0530

    hospitals

--- src/pages/Hospitals.tsx
+++ src/pages/Hospitals.tsx
@@ -1,12 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonFab, IonFabButton, IonFabList, IonIcon, IonItem, IonBadge, IonLabel, IonAlert, useIonViewWillEnter, IonCard } from '@ionic/react';
-import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonFab, IonFabButton, IonFabList, IonIcon, IonItem, IonBadge, IonLabel, IonAlert, useIonViewWillEnter, IonCard,IonCardTitle,IonCardContent,IonCardSubtitle,IonCardHeader, IonInfiniteScroll } from '@ionic/react';
+import React, { useState, useEffect } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Dashboard.css';
 import { getCurrentUser } from '../firebaseConfig';
 import { Redirect, useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 import { logoutUser } from '../firebaseConfig'
-import { eyedropOutline, barChartOutline, heartOutline, personOutline, analyticsOutline } from 'ionicons/icons';
+import { eyedropOutline, barChartOutline, heartOutline, personOutline, analyticsOutline, key } from 'ionicons/icons';
 import { hostname } from 'os';
 import Vaccines from './Vaccines';
 import { State } from 'ionicons/dist/types/stencil-public-runtime';
@@ -16,20 +16,29 @@ const Hospitals: React.FC = () => {
 	const [showAlert, setShowAlert] = useState(false);
 
 	//Basically, data aa raha hai but it isnt being saved to this state figure out what type the data is and use proper brackets in the state
-	const [hospital, setHospital] = useState((''))
+	const [hospital, setHospital] = useState([{
+		description:'',
+		location:'',
+		name:'',
+		vaccines:'',
+		id:''
+	}
+
+	])
+
+
+	 useEffect(()=>{
+		axios.get('https://api.arhaanb.co/cura/hospitals')
+		.then((res: any) => {
+			setHospital(res.data)
+		})
+	},[])
+	console.log(hospital)
+	const data = hospital
 
 
 	async function dataloader() {
-		await axios.get('https://api.arhaanb.co/cura/hospitals')
-			.then((res: any) => {
 
-				//this state isnt being saved
-				setHospital(res)
-				console.log(hospital)
-
-				//the data is being logged
-				console.log(res.data[0])
-			})
 	}
 
 	//load data every time view mounts
@@ -50,10 +59,23 @@ const Hospitals: React.FC = () => {
 					subHeader={'hjh'}
 					message={'Feel Free to Visit!'}
 					buttons={['Okay']} />
-				<IonButton onClick={() => setShowAlert(true)} expand="block">ANUS Alert 2</IonButton>
 			</IonContent>
 
-			<p>{}</p>
+			{hospital.map(hospital=>(
+				<IonInfiniteScroll>
+				<IonCard color='dark'>
+				<IonCardHeader>
+					<IonCardTitle key={hospital.name}>{hospital.name}</IonCardTitle>
+					<IonCardSubtitle key={hospital.location}>{hospital.location}</IonCardSubtitle>
+					<IonCardContent key={hospital.description}>{hospital.description}</IonCardContent>
+					<IonButton color='secondary' onClick={() => setShowAlert(true)}>More</IonButton>
+				</IonCardHeader>
+				
+				</IonCard>
+				</IonInfiniteScroll>
+			))}
+
+
 
 			<IonFab slot='fixed' vertical='bottom' horizontal='end'>
 				<IonFabButton>
